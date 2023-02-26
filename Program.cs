f2ed60dab5eb78e3402ec021e7c0617f6b2ea944
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-02-26 21:13:58 +0100

    added else statements

--- Program.cs
+++ Program.cs
@@ -8,4 +8,16 @@ string? lastName = Console.ReadLine();
 if (firstName.ToLower() == "josh" && lastName.ToLower() == "o'connor")
 {
     Console.WriteLine("Hello Mr. O'Connor");
-} 
+}
+else if (lastName.ToLower() == "o'connor")
+{
+    Console.WriteLine("You have a cool last  name!");
+}
+else if (firstName.ToLower() == "josh")
+{
+    Console.WriteLine("You have a great first name!!");
+}
+else
+{
+    Console.WriteLine("Sorry you dont have a cooler name! :P");
+}
