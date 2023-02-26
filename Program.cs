48bb411c42ae2c5d8e4598523547936e809500c4
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-02-26 21:05:18 +0100

    added in if statement

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.Write("What is your first name?: ");
+string? firstName = Console.ReadLine();
+
+Console.Write("What is your last name?: ");
+string? lastName = Console.ReadLine();
+
+if (firstName.ToLower() == "josh" && lastName.ToLower() == "o'connor")
+{
+    Console.WriteLine("Hello Mr. O'Connor");
+} 
